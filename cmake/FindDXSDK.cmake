fdcaddd7b9e995bab8c8f0693e3ebad0e16356e0
Dario Mariani <dario.mariani@autodesk.com>
2020-08-07 14:52:48 -0300

    Fix DX11 SDK search.

--- cmake/FindDXSDK.cmake
+++ cmake/FindDXSDK.cmake
@@ -34,61 +34,45 @@
 # Also will define
 
 if (WIN32)
+    # Find the win10 SDK path.
+    if ("$ENV{WIN10_SDK_PATH}$ENV{WIN10_SDK_VERSION}" STREQUAL "" )
+      get_filename_component(WIN10_SDK_PATH "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Microsoft SDKs\\Windows\\v10.0;InstallationFolder]" ABSOLUTE CACHE)
+      get_filename_component(TEMP_WIN10_SDK_VERSION "[HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Microsoft SDKs\\Windows\\v10.0;ProductVersion]" ABSOLUTE CACHE)
+      get_filename_component(WIN10_SDK_VERSION ${TEMP_WIN10_SDK_VERSION} NAME)
+    else()
+      set (WIN10_SDK_PATH $ENV{WIN10_SDK_PATH})
+      set (WIN10_SDK_VERSION $ENV{WIN10_SDK_VERSION})
+    endif()
+    if (IS_DIRECTORY "${WIN10_SDK_PATH}/Include/${WIN10_SDK_VERSION}.0")
+      set(WIN10_SDK_VERSION "${WIN10_SDK_VERSION}.0")
+    endif()
+
     find_path(DXSDK_INCLUDE_DIR
         NAMES
             D3D11.h D3Dcompiler.h
         PATHS
-            "${DXSDK_LOCATION}/Include"
-            "$ENV{DXSDK_LOCATION}/Include"
-            "${DXSDK_ROOT}/Include"
-            "$ENV{DXSDK_ROOT}/Include"
-            "C:/Program Files (x86)/Windows Kits/10/Include/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/um"
-            "C:/Program Files (x86)/Windows Kits/8.1/Include/um"
-            "C:/Program Files (x86)/Microsoft DirectX SDK*/Include"
-            "C:/Program Files/Microsoft DirectX SDK*/Include"
+            "${WIN10_SDK_PATH}/Include/${WIN10_SDK_VERSION}"
     )
-
-    if ("${CMAKE_GENERATOR}" MATCHES "[Ww]in64" OR
-        "${CMAKE_GENERATOR_PLATFORM}" MATCHES "x64")
-        set(ARCH x64)
-    else()
-        set(ARCH x86)
-    endif()
-
-    find_path(LIBRARY_DIR
-            d3d11.lib
-        PATHS
-            "${DXSDK_LOCATION}/Lib/${ARCH}"
-            "$ENV{DXSDK_LOCATION}/Lib/${ARCH}"
-            "${DXSDK_ROOT}/Lib/${ARCH}"
-            "$ENV{DXSDK_ROOT}/Lib/${ARCH}"
-            "C:/Program Files (x86)/Windows Kits/10/Lib/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/um/${ARCH}"
-            "C:/Program Files (x86)/Windows Kits/8.1/Lib/winv6.3/um/${ARCH}"
-            "C:/Program Files (x86)/Microsoft DirectX SDK*/Lib/${ARCH}"
-            "C:/Program Files/Microsoft DirectX SDK*/Lib/${ARCH}"
-    )
-
-    set(DXSDK_LIBRARY_DIR ${LIBRARY_DIR})
 
     foreach(DX_LIB d3d11 d3dcompiler)
-
         find_library(DXSDK_${DX_LIB}_LIBRARY
             NAMES 
                 ${DX_LIB}.lib
             PATHS
-                ${DXSDK_LIBRARY_DIR}
-            NO_DEFAULT_PATH
+                "${WIN10_SDK_PATH}/Lib/${WIN10_SDK_VERSION}/um/x64"
+                "${WIN10_SDK_PATH}/Lib/${WIN10_SDK_VERSION}/um/x86"
         )
-
         if(NOT DXSDK_${DX_LIB}_LIBRARY)
             message(FATAL_ERROR "Could not find required library ${DX_LIB}")
         else()
             list(APPEND DXSDK_LIBRARIES ${DXSDK_${DX_LIB}_LIBRARY})
         endif()
-
-
     endforeach(DX_LIB)
 
+    list(GET DXSDK_LIBRARIES 0 D3D11_LIBRARY)
+    get_filename_component(DXSDK_LIBRARY_DIR ${D3D11_LIBRARY} DIRECTORY)
+    message (STATUS "DX11 headers path:   ${DXSDK_INCLUDE_DIR}")
+    message (STATUS "DX11 libraries path: ${DXSDK_LIBRARY_DIR}")
 endif ()
 
 include(FindPackageHandleStandardArgs)
@@ -104,4 +88,3 @@ mark_as_advanced(
     DXSDK_LIBRARY_DIR
     DXSDK_LIBRARIES
 )
-
