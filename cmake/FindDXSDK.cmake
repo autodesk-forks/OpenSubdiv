ca012e99fc133e3be9921417cd0ff6cc78f535b3
Dario Mariani <dario.mariani@autodesk.com>
2020-08-11 15:00:55 -0300

    Fix DirectX 11 lookup.

--- cmake/FindDXSDK.cmake
+++ cmake/FindDXSDK.cmake
@@ -27,7 +27,6 @@
 #
 # DXSDK_FOUND
 # DXSDK_INCLUDE_DIR
-# DXSDK_LIBRARY_DIR
 # DXSDK_LIBRARIES
 # DXSDK_LOCATION
 #
@@ -37,47 +36,13 @@ if (WIN32)
     find_path(DXSDK_INCLUDE_DIR
         NAMES
             D3D11.h D3Dcompiler.h
-        PATHS
-            "${DXSDK_LOCATION}/Include"
-            "$ENV{DXSDK_LOCATION}/Include"
-            "${DXSDK_ROOT}/Include"
-            "$ENV{DXSDK_ROOT}/Include"
-            "C:/Program Files (x86)/Windows Kits/10/Include/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/um"
-            "C:/Program Files (x86)/Windows Kits/8.1/Include/um"
-            "C:/Program Files (x86)/Microsoft DirectX SDK*/Include"
-            "C:/Program Files/Microsoft DirectX SDK*/Include"
     )
-
-    if ("${CMAKE_GENERATOR}" MATCHES "[Ww]in64" OR
-        "${CMAKE_GENERATOR_PLATFORM}" MATCHES "x64")
-        set(ARCH x64)
-    else()
-        set(ARCH x86)
-    endif()
-
-    find_path(LIBRARY_DIR
-            d3d11.lib
-        PATHS
-            "${DXSDK_LOCATION}/Lib/${ARCH}"
-            "$ENV{DXSDK_LOCATION}/Lib/${ARCH}"
-            "${DXSDK_ROOT}/Lib/${ARCH}"
-            "$ENV{DXSDK_ROOT}/Lib/${ARCH}"
-            "C:/Program Files (x86)/Windows Kits/10/Lib/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/um/${ARCH}"
-            "C:/Program Files (x86)/Windows Kits/8.1/Lib/winv6.3/um/${ARCH}"
-            "C:/Program Files (x86)/Microsoft DirectX SDK*/Lib/${ARCH}"
-            "C:/Program Files/Microsoft DirectX SDK*/Lib/${ARCH}"
-    )
-
-    set(DXSDK_LIBRARY_DIR ${LIBRARY_DIR})
 
     foreach(DX_LIB d3d11 d3dcompiler)
 
         find_library(DXSDK_${DX_LIB}_LIBRARY
             NAMES 
                 ${DX_LIB}.lib
-            PATHS
-                ${DXSDK_LIBRARY_DIR}
-            NO_DEFAULT_PATH
         )
 
         if(NOT DXSDK_${DX_LIB}_LIBRARY)
@@ -85,7 +50,6 @@ if (WIN32)
         else()
             list(APPEND DXSDK_LIBRARIES ${DXSDK_${DX_LIB}_LIBRARY})
         endif()
-
 
     endforeach(DX_LIB)
 
@@ -95,13 +59,10 @@ include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(DXSDK DEFAULT_MSG
     DXSDK_INCLUDE_DIR
-    DXSDK_LIBRARY_DIR
     DXSDK_LIBRARIES
 )
 
 mark_as_advanced(
     DXSDK_INCLUDE_DIR
-    DXSDK_LIBRARY_DIR
     DXSDK_LIBRARIES
 )
-
